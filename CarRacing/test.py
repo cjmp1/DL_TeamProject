406d2dbd0942844c100b1461afeb25f674c44aa8
cjmp1 <chlwjdals1996@gmail.com>
2019-11-29 13:11:28 +0900

    Add files via upload

---
+++ CarRacing/test.py
@@ -0,0 +1,23 @@
+import gym
+import numpy as np
+import torch
+
+from nn.conv import ConvNet
+
+if __name__ == '__main__':
+    env = gym.make('CarRacing-v0')
+    obs = env.reset()
+    nn = ConvNet()
+    nn.load("./09-26-2019_16-44_NN=ConvNetTorchIndividal_POPSIZE=100_GEN=1000_PMUTATION_0.2_PCROSSOVER_0.8.npy")
+
+    for _ in range(1000):
+        env.render()
+        obs = torch.from_numpy(np.flip(obs, axis=0).copy()).float()
+        obs = obs.reshape((-1, 3, 96, 96))
+        action = nn.forward(obs)
+        action = action.detach().numpy()
+        obs, reward, done, _ = env.step(action)
+        if done:
+            break
+
+    env.close()
